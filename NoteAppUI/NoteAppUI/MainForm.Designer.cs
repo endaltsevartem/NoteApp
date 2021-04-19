84f0e20a5dc9526f70eba4153329e537174640cd
артём ендальцев <armonishe@mail.ru>
2021-04-19 18:17:54 +0700

    Revert "переименование form1 в mainform"
    This reverts commit 5a10faee1d1fc8f68bbd5e65572d86fab02a9f92.

--- NoteAppUI/NoteAppUI/MainForm.Designer.cs
+++ NoteAppUI/NoteAppUI/MainForm.Designer.cs
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "MainForm";
-            this.Text = "MainForm";
-            this.ResumeLayout(false);
-
+            this.Text = "Form1";
         }
 
         #endregion
